47fd5f6b3c387dfa80bc1fff35b80b35ed52412a
Michael Ingold <michael.ingold@accenture.com>
2023-07-25 22:23:07 +0200

    added endpoint to patch multiple resources using filters

--- src/Manifesto.AspNet/Controllers/ManifestV1PatchController.cs
+++ src/Manifesto.AspNet/Controllers/ManifestV1PatchController.cs
@@ -1,4 +1,5 @@
 using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 using dotnet_etcd.interfaces;
 using Manifesto.AspNet.Manifests;
@@ -11,7 +12,7 @@ using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace Manifesto.AspNet.Controllers;
 [ApiController]
-[Route("apis/{group}/{version}/{kind}/{name}")]
+[Route("apis/{group}/{version}/{kind}/")]
 public class ManifestV1PatchController : ControllerBase
 {
     private readonly IEtcdClient _etcdClient;
@@ -29,13 +30,13 @@ public class ManifestV1PatchController : ControllerBase
         this.keyspaceSource = keyspaceSource;
     }
 
-    [HttpPatch(Name = "PatchResourceName")]
+    [HttpPatch("{name}", Name = "PatchResourceName")]
     public async Task<IActionResult> PatchResourceByName(
         string group,
         string version,
         string kind,
         string name,
-        [FromBody]JsonPatchDocument<ManifestPatch> patchDoc,
+        [FromBody] JsonPatchDocument<ManifestPatch> patchDoc,
         CancellationToken cancellationToken)
     {
         var keyspace = keyspaceSource.GetKeySpace(kind, version, group);
@@ -46,14 +47,16 @@ public class ManifestV1PatchController : ControllerBase
         try
         {
             var response = await _etcdClient.GetAsync(key, cancellationToken: cancellationToken);
-            if(response.Count == 0) {
+            if (response.Count == 0)
+            {
                 return NotFound();
             }
 
             var first_response = response.Kvs.First();
             var first = JsonConvert.DeserializeObject<ManifestPatch>(first_response.Value.ToStringUtf8());
 
-            try {
+            try
+            {
                 patchDoc.ApplyTo(first);
                 var serializedManifest = JsonSerializer.Serialize(first);
 
@@ -67,10 +70,12 @@ public class ManifestV1PatchController : ControllerBase
                 }
 
                 return Ok(first);
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 return StatusCode(StatusCodes.Status500InternalServerError, $"Failed to apply patch: {e.Message}");
             }
-            
+
         }
         catch (Exception e)
         {
@@ -79,7 +84,60 @@ public class ManifestV1PatchController : ControllerBase
         }
     }
 
-    
+    [HttpPatch(Name = "PatchResourcesBatched")]
+    public async Task<IActionResult> PatchResourcesBatched(
+        string group,
+        string version,
+        string kind,
+        string filter,
+        [FromBody] JsonPatchDocument<ManifestPatch> patchDoc,
+        CancellationToken cancellationToken)
+    {
+        var keyspace = keyspaceSource.GetKeySpace(kind, version, group);
+        if (string.IsNullOrWhiteSpace(keyspace)) { return NotFound($"no resource type know for group: {group}, version: {version}, kind: {kind}"); }
+
+        var filters = Utilities.Selectors.TryParse(filter);
+        if (!filters.Any())
+        {
+            return BadRequest("failed to parse filter string");
+        }
+
+        try
+        {
+            var response = await _etcdClient.GetRangeAsync(keyspace, cancellationToken: cancellationToken);
+            var manifests = response.Kvs
+                .Select(x => JsonConvert.DeserializeObject<ManifestPatch>(x.Value.ToStringUtf8()))
+                .Where(x => Utilities.Selectors.Validate(filters, x.Metadata.Labels))
+                .Select(x => {
+                    patchDoc.ApplyTo(x);
+                    return x;
+                })
+                .ToArray();
+            
+            var tasks = manifests.Select(x => {
+                var serializedManifest = JsonSerializer.Serialize(x);
+                var key = Path.Combine(keyspace, x.Metadata.Name);
+                return _etcdClient.PutAsync(key, serializedManifest, cancellationToken: cancellationToken);
+            });
+
+            try
+            {
+                await Task.WhenAll(tasks);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Failed to apply patch: {e.Message}");
+            }
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, $"Failed to get resource {keyspace}");
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Failed to apply patch: {e.Message}");
+        }
+
+        return Ok();
+    }
+
     public class MetadataPatch
     {
         public string Name { get; set; }
